26a521bfd27b7c08e7b4f60c032068704c890545
VladimirLesh <v.lesh@icloud.com>
2022-12-04 23:11:02 +0300

    first commit

---
+++ Assets/Scripts/TownBuilder.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownBuilder : MonoBehaviour
+{
+    [SerializeField] private int _levelCount;
+    [SerializeField] private float _additionalScale;
+    [SerializeField] private GameObject _beam;
+    [SerializeField] private StartPlatform _startPlatform;
+    [SerializeField] private FinishPlatform _finishPlatform;
+    [SerializeField] private Platform[] _platforms;
+
+    private float _startAndFinishAdditionalScale = 1f;
+
+    public float BeamScaleY => _levelCount / 7f + _startAndFinishAdditionalScale - _additionalScale / 8f;
+
+    private void Start()
+    {
+        Build();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void Build()
+    {
+        GameObject beam = Instantiate(_beam, transform);
+        beam.transform.localScale = new Vector3(1, _levelCount / BeamScaleY, 1);
+        Vector3 spawnPosition = beam.transform.position;
+        spawnPosition.y += beam.transform.localScale.y - _additionalScale;
+
+        SpawnPlatform(_startPlatform, ref spawnPosition, beam.transform);
+        for (int i = 0; i < _levelCount; i++)
+        {
+            SpawnPlatform(_platforms[Random.Range(0, _platforms.Length)], ref spawnPosition, beam.transform);
+        }
+        SpawnPlatform(_finishPlatform, ref spawnPosition, beam.transform);
+    }
+
+    private void SpawnPlatform(Platform platform, ref Vector3 spawnPosition, Transform parent)
+    {
+        Instantiate(platform, spawnPosition, Quaternion.Euler(0, Random.Range(0, 360), 0), parent);
+        spawnPosition.y -= 1f;
+    }
+
+}
