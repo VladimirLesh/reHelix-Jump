fa460d5177f9a2ef5c342ad0605ded03a26325c7
VladimirLesh <v.lesh@icloud.com>
2022-12-07 00:30:55 +0300

    Create first level

--- Assets/Scripts/Platform/Platform.cs
+++ Assets/Scripts/Platform/Platform.cs
@@ -4,4 +4,16 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+    [SerializeField] private float _bounceForse;
+    [SerializeField] private float _bounceRadius;
+
+    public void Break()
+    {
+        PlatformSegment[] platformSegments = GetComponentsInChildren<PlatformSegment>();
+
+        foreach (var item in platformSegments)
+        {
+            item.Bounce(_bounceForse, transform.position, _bounceRadius);
+        }
+    }
 }
